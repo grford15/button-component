8ddb1103a3348d02463e2d483711481aa2c0f4c9
Greg <g.rutherford10@gmail.com>
2020-11-16 16:19:55 +0000

    create two default buttons with styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1 id="title">Buttons</h1>
+      <div id="button-container">
+        <div className="button-pair">
+          <code>{"<Button />"}</code>
+          <code>{"&:hover, &:focus"}</code>
+          <button className="default">Default</button>
+          <button className="default default-hover">Default</button>
+        </div>
+      </div>
+      <footer>
+        <p>Created by: Greg Rutherford</p>
+      </footer>
     </div>
   );
 }
